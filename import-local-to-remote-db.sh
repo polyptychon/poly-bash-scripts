d3f0acb706c55089934f46e65b0ec0d74d5f2076
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2015-03-12 17:09:47 +0200

    update scripts

--- import-local-to-remote-db.sh
+++ import-local-to-remote-db.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 source .env
-# expect -c "
-# spawn mysqldump -p -u $LOCAL_DATABASE_USERNAME $LOCAL_DATABASE
-# expect -nocase \"password:\" {send \"$LOCAL_DATABASE_PASSWORD\r\"; interact}
-# " > exports/temp.sql
 wp db export exports/temp.sql --path=./wordpress
 sed 's/-- Dump completed on....................//g' exports/temp.sql > exports/local.sql
 sed "s/$LOCAL_DOMAIN/$REMOTE_DOMAIN/g" exports/local.sql > exports/remote.sql
+scp -rCP 2222 exports/remote.sql "$SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/exports/temp.sql"
