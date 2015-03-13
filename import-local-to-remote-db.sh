5715589f81491edcee73bec4a356775ff14faf92
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2015-03-13 14:40:27 +0200

    clean up

--- import-local-to-remote-db.sh
+++ import-local-to-remote-db.sh
@@ -8,16 +8,16 @@ then
 backup-remote-db.sh
 
 # import local db to remote
-wp db export $PATH_TO_EXPORTS/temp.sql --path=$PATH_TO_WORDPRESS
-sed -e '/-- Dump completed on/d;/-- MySQL dump/d;/-- Host\: /d;/-- Server version/d' $PATH_TO_EXPORTS/temp.sql > $PATH_TO_EXPORTS/local.sql
-sed "s/$LOCAL_DOMAIN/$REMOTE_DOMAIN/g" $PATH_TO_EXPORTS/local.sql > $PATH_TO_EXPORTS/remote.sql
-scp -rCP $SSH_PORT $PATH_TO_EXPORTS/remote.sql "$SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_EXPORTS/temp.sql"
+wp db export $PATH_TO_EXPORTS/temp.sql --path=$PATH_TO_WORDPRESS --skip-dump-date
+sed -e '/-- Dump completed on/d;/-- MySQL dump/d;/-- Host\: /d;/-- Server version/d' $PATH_TO_EXPORTS/temp.sql > $PATH_TO_EXPORTS/local.temp.sql
+sed "s/$LOCAL_DOMAIN/$REMOTE_DOMAIN/g" $PATH_TO_EXPORTS/local.temp.sql > $PATH_TO_EXPORTS/remote.temp.sql
+scp -rCP $SSH_PORT $PATH_TO_EXPORTS/remote.temp.sql "$SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_EXPORTS/temp.sql"
 
 ssh -p $SSH_PORT $SSH_USERNAME@$SSH_HOST bash -c "'
 cd $REMOTE_PATH
 wp db import $PATH_TO_EXPORTS/temp.sql --path=$PATH_TO_WORDPRESS
 exit
 '"
-git checkout $PATH_TO_EXPORTS/local.sql
-git checkout $PATH_TO_EXPORTS/remote.sql
+rm -rf exports/local.temp.sql
+rm -rf exports/remote.temp.sql
 fi
--- import-remote-to-local-db.sh
+++ import-remote-to-local-db.sh
@@ -3,12 +3,12 @@ set -e
 source .env
 ssh -p $SSH_PORT $SSH_USERNAME@$SSH_HOST bash -c "'
 cd $REMOTE_PATH
-wp db export $PATH_TO_EXPORTS/temp.sql --path=$PATH_TO_WORDPRESS
+wp db export $PATH_TO_EXPORTS/temp.sql --path=$PATH_TO_WORDPRESS --skip-dump-date
 exit
 '"
 scp -CP $SSH_PORT $SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_EXPORTS/temp.sql $PATH_TO_EXPORTS/
-sed -e '/-- Dump completed on/d;/-- MySQL dump/d;/-- Host\: /d;/-- Server version/d' $PATH_TO_EXPORTS/temp.sql > $PATH_TO_EXPORTS/remote.sql
-sed "s/$REMOTE_DOMAIN/$LOCAL_DOMAIN/g" $PATH_TO_EXPORTS/remote.sql > $PATH_TO_EXPORTS/local.sql
-wp db import $PATH_TO_EXPORTS/local.sql --path=$PATH_TO_WORDPRESS
-git checkout $PATH_TO_EXPORTS/remote.sql
-git checkout $PATH_TO_EXPORTS/local.sql
+sed -e '/-- Dump completed on/d;/-- MySQL dump/d;/-- Host\: /d;/-- Server version/d' $PATH_TO_EXPORTS/temp.sql > $PATH_TO_EXPORTS/remote.temp.sql
+sed "s/$REMOTE_DOMAIN/$LOCAL_DOMAIN/g" $PATH_TO_EXPORTS/remote.temp.sql > $PATH_TO_EXPORTS/local.temp.sql
+wp db import $PATH_TO_EXPORTS/local.temp.sql --path=$PATH_TO_WORDPRESS
+rm -rf exports/local.temp.sql
+rm -rf exports/remote.temp.sql
