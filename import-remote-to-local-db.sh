fb5cf96b43b9528739af4cdbb5a5e7f983be31ed
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2015-03-14 17:30:03 +0200

    fix contact-form-7 import

--- import-remote-to-local-db.sh
+++ import-remote-to-local-db.sh
@@ -3,13 +3,14 @@ set -e
 source .env
 ssh -p $SSH_PORT $SSH_USERNAME@$SSH_HOST bash -c "'
 cd $REMOTE_PATH
-wp db export $PATH_TO_EXPORTS/temp.sql --path=$PATH_TO_WORDPRESS --skip-dump-date --compact --add-drop-table
+wp db export $PATH_TO_EXPORTS/temp.sql --path=$PATH_TO_WORDPRESS  --skip-comments --skip-dump-date --skip-opt --add-drop-table
 exit
 '"
 scp -CP $SSH_PORT $SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_EXPORTS/temp.sql $PATH_TO_EXPORTS/
 sed -e '/-- Dump completed on/d;/-- MySQL dump/d;/-- Host\: /d;/-- Server version/d' $PATH_TO_EXPORTS/temp.sql > $PATH_TO_EXPORTS/remote.temp.sql
-sed "s/$REMOTE_DOMAIN/$LOCAL_DOMAIN/g" $PATH_TO_EXPORTS/remote.temp.sql > $PATH_TO_EXPORTS/local.temp.sql
+sed -e "s/$REMOTE_DOMAIN/$LOCAL_DOMAIN/g;s/\<wordpress@$LOCAL_DOMAIN\>/\<wordpress@$REMOTE_DOMAIN\>/g" $PATH_TO_EXPORTS/remote.temp.sql > $PATH_TO_EXPORTS/local.temp.sql
 wp db import $PATH_TO_EXPORTS/local.temp.sql --path=$PATH_TO_WORDPRESS
+#mysql -p -u $LOCAL_DATABASE_USERNAME $LOCAL_DATABASE < $PATH_TO_EXPORTS/local.temp.sql
 rm -rf exports/local.temp.sql
 rm -rf exports/remote.temp.sql
 rm -rf exports/temp.sql
