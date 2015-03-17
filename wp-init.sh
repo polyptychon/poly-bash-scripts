ab25570ff47a736ca2c727306294de34e10be023
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2015-03-17 09:10:58 +0200

    update wp init

--- wp-init.sh
+++ wp-init.sh
@@ -4,37 +4,88 @@ if [ -f ~/wp_scripts/.global-env ]; then
   source ~/wp_scripts/.global-env
 fi
 
-DIR_NAME=${PWD##*/}
-WP_SITE_TITLE=$DIR_NAME
-WP_USER_PASSWORD="$(openssl rand -base64 16)"
-
 if [ -f wp-cli.local.yml ]; then
   echo "ERROR: wp-cli.local.yml file already exists"
   exit;
 fi
 
+DIR_NAME=${PWD##*/}
+WP_SITE_TITLE=$DIR_NAME
+WP_USER_PASSWORD="$(date | md5)"
+DB_NAME="$DIR_NAME"_db
+WP_USER=admin
+
 #Prompt user for settings
 while (true); do
 
-  echo -n " # Local Database name: "
-  read DB_NAME
-
-  if [ -z ${DB_USER+x} ]; then
-    echo -n " # Local Database user: "
-    read DB_USER
+  echo -n " # SSH HOST ($SSH_HOST): "
+  read SSH_HOST_TEMP
+  if [ ! -z ${SSH_HOST_TEMP} ]; then
+    SSH_HOST=$SSH_HOST_TEMP
   fi
 
-  if [ -z ${DB_PASSWORD+x} ]; then
-    echo -n " # Local Database password: "
-    read DB_PASSWORD
+  echo -n " # SSH PORT ($SSH_PORT): "
+  read SSH_PORT_TEMP
+  if [ ! -z ${SSH_PORT_TEMP} ]; then
+    SSH_PORT=$SSH_PORT_TEMP
   fi
 
-  echo -n " # Wordpress admin user: "
-  read WP_USER
+  echo -n " # SSH USERNAME ($SSH_USERNAME): "
+  read SSH_USERNAME_TEMP
+  if [ ! -z ${SSH_USERNAME_TEMP} ]; then
+    SSH_USERNAME=$SSH_USERNAME_TEMP
+  fi
 
-  echo " # Wordpress admin password: $WP_USER_PASSWORD"
+  echo -n " # Remote SSH domains root path ($REMOTE_SSH_ROOT_PATH): "
+  read REMOTE_SSH_ROOT_PATH_TEMP
+  if [ ! -z ${REMOTE_SSH_ROOT_PATH_TEMP} ]; then
+    REMOTE_SSH_ROOT_PATH=$REMOTE_SSH_ROOT_PATH_TEMP
+  fi
+
+  echo -n " # Relative path to wordpress ($PATH_TO_WORDPRESS): "
+  read PATH_TO_WORDPRESS_TEMP
+  if [ ! -z ${PATH_TO_WORDPRESS_TEMP} ]; then
+    PATH_TO_WORDPRESS=$PATH_TO_WORDPRESS_TEMP
+  fi
+
+  echo -n " # Relative path to exports ($PATH_TO_EXPORTS): "
+  read PATH_TO_EXPORTS_TEMP
+  if [ ! -z ${PATH_TO_EXPORTS_TEMP} ]; then
+    PATH_TO_EXPORTS=$PATH_TO_EXPORTS_TEMP
+  fi
+
+  echo -n " # Local Database name ($DB_NAME): "
+  read DB_NAME_TEMP
+  if [ ! -z ${DB_NAME_TEMP} ]; then
+    DB_NAME=$DB_NAME_TEMP
+  fi
+
+  echo -n " # Local Database user ($DB_USER): "
+  read DB_USER_TEMP
+  if [ ! -z ${DB_USER_TEMP} ]; then
+    DB_USER=$DB_USER_TEMP
+  fi
+
+  echo -n " # Local Database password ($DB_PASSWORD): "
+  read DB_PASSWORD_TEMP
+  if [ ! -z ${DB_PASSWORD_TEMP} ]; then
+    DB_PASSWORD=$DB_PASSWORD_TEMP
+  fi
+
+  echo -n " # Wordpress admin user ($WP_USER): "
+  read WP_USER_TEMP
+  if [ ! -z ${WP_USER_TEMP} ]; then
+    WP_USER=$WP_USER_TEMP
+  fi
+
+  echo -n " # Wordpress admin password ($WP_USER_PASSWORD):"
+  read WP_USER_PASSWORD
+  if [ ! -z ${WP_USER_PASSWORD_TEMP} ]; then
+    WP_USER_PASSWORD=$WP_USER_PASSWORD_TEMP
+  fi
+
   FOLDER="$(pwd)"
-  echo -n "You are in folder $FOLDER.Do you want to continue? [y/n]: "
+  echo -n "You are in folder $FOLDER. Do you want to continue? [y/n]: "
   read answer
   if [[ $answer == "y" ]]; then
     break;
@@ -50,10 +101,16 @@ if [ $PASSWORD_IS_OK == 0 ]; then
   exit;
 fi
 
-if [ ! -f ~/wp_scripts/.global-env ]; then
-  echo "DB_USER=$DB_USER" > ~/wp_scripts/.global-env
-  echo "DB_PASSWORD=$DB_PASSWORD" >> ~/wp_scripts/.global-env
-fi
+
+echo "SSH_HOST=$SSH_HOST" > ~/wp_scripts/.global-env
+echo "SSH_PORT=$SSH_PORT" >> ~/wp_scripts/.global-env
+echo "SSH_USERNAME=$SSH_USERNAME" >> ~/wp_scripts/.global-env
+echo "DB_USER=$DB_USER" >> ~/wp_scripts/.global-env
+echo "DB_PASSWORD=$DB_PASSWORD" >> ~/wp_scripts/.global-env
+echo "REMOTE_SSH_ROOT_PATH=$REMOTE_SSH_ROOT_PATH" >> ~/wp_scripts/.global-env
+echo "PATH_TO_WORDPRESS=$PATH_TO_WORDPRESS" >> ~/wp_scripts/.global-env
+echo "PATH_TO_EXPORTS=$PATH_TO_EXPORTS" >> ~/wp_scripts/.global-env
+
 
 if [ ! -f wp-admin-password.txt ]; then
   echo "WP_USER_PASSWORD: $WP_USER_PASSWORD" > ./wp-admin-password.txt
@@ -63,7 +120,7 @@ else
 fi
 
 if [ ! -f wp-cli.local.yml ]; then
-  echo "path: wordpress" > ./wp-cli.local.yml
+  echo "path: $PATH_TO_WORDPRESS" > ./wp-cli.local.yml
   echo "url: http://$DIR_NAME.local:8888" >> ./wp-cli.local.yml
   echo "user: $WP_USER" >> ./wp-cli.local.yml
 else
@@ -72,14 +129,14 @@ else
 fi
 
 if [ ! -f .env ]; then
-  echo "SSH_HOST=polyptychon.gr" > .env
-  echo "SSH_PORT=2222" >> .env
-  echo "SSH_USERNAME=xarisd" >> .env
-  echo "REMOTE_DOMAIN=$DIR_NAME.polyptychon.gr" >> .env
-  echo "REMOTE_DOMAIN=$DIR_NAME.local:8888" >> .env
-  echo "REMOTE_PATH=./domains/$DIR_NAME" >> .env
-  echo "PATH_TO_WORDPRESS=./wordpress" >> .env
-  echo "PATH_TO_EXPORTS=./exports" >> .env
+  echo "SSH_HOST=$SSH_HOST" > .env
+  echo "SSH_PORT=$SSH_PORT" >> .env
+  echo "SSH_USERNAME=$SSH_USERNAME" >> .env
+  echo "REMOTE_DOMAIN=$DIR_NAME.$SSH_HOST" >> .env
+  echo "LOCAL_DOMAIN=$DIR_NAME.local:8888" >> .env
+  echo "REMOTE_PATH=$REMOTE_SSH_ROOT_PATH/$DIR_NAME" >> .env
+  echo "PATH_TO_WORDPRESS=$PATH_TO_WORDPRESS" >> .env
+  echo "PATH_TO_EXPORTS=$PATH_TO_EXPORTS" >> .env
 else
   echo "WARNING: .env file already exists"
   exit;
