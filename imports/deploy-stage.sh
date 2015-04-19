ae2c2ba36c3b78adfbc6f37ab3f4745d8b4df283
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2015-04-19 09:41:22 +0300

    deploy stage

--- imports/deploy-stage.sh
+++ imports/deploy-stage.sh
@@ -6,12 +6,26 @@ set -e
 # load variables
 source .env
 
+DIR_NAME=${PWD##*/}
+
 ssh -t -p 2222 xarisd@polyptychon.gr bash -c "'
 
-cd $REMOTE_PATH
-git status
-git pull
-exit
+if [[ -d $REMOTE_PATH ]]; then
+
+  cd $REMOTE_PATH
+  git stash --quiet
+  git status
+  git pull
+  git stash pop --quiet
+  exit
+
+else
+
+  cd $REMOTE_SSH_ROOT_PATH
+  pwd
+  git clone git@github.com:polyptychon/$DIR_NAME.git
+
+fi
 
 '"
 
