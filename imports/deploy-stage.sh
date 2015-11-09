77bda65d1f434b6e6ce6cc3285ebd6a8e0f3091c
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2015-11-09 14:01:40 +0200

    clean up

--- imports/deploy-stage.sh
+++ imports/deploy-stage.sh
@@ -123,25 +123,6 @@ else
       echo \"</IfModule>\" >> $PATH_TO_WORDPRESS/.htaccess
       echo \"\" >> $PATH_TO_WORDPRESS/.htaccess
       echo \"# END WordPress\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"# BEGIN Optimization\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"<IfModule mod_deflate.c>\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"<FilesMatch \"\\.(js|css|html|htm|php|xml|svg)$\">\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"SetOutputFilter DEFLATE\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"</FilesMatch>\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"</IfModule>\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"# 480 weeks\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"<FilesMatch \"\.(ico|pdf|flv|jpg|jpeg|png|gif|js|css|swf|woff|svg)$\">\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"Header set Cache-Control \"max-age=290304000, public\"\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"</FilesMatch>\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"# 2 DAYS\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"<FilesMatch \"\.(xml|txt)$\">\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"Header set Cache-Control \"max-age=172800, public, must-revalidate\"\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"</FilesMatch>\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"# 2 HOURS\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"<FilesMatch \"\.(html|htm)$\">\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"Header set Cache-Control \"max-age=7200, must-revalidate\"\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"</FilesMatch>\" >> $PATH_TO_WORDPRESS/.htaccess
-      echo \"# END Optimization\" >> $PATH_TO_WORDPRESS/.htaccess
       echo \"\" >> $PATH_TO_WORDPRESS/.htaccess
       wp rewrite flush
     fi
