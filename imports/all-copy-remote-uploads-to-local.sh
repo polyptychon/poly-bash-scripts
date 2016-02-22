c9893d228e5fc9fbac998d04cdb5c3e399fbb136
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-02-22 11:44:10 +0200

    clean up

--- imports/all-copy-remote-uploads-to-local.sh
+++ imports/all-copy-remote-uploads-to-local.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function clean_up {
+  ssh -O exit -o ControlPath="$HOME/.ssh/ctl/%L-%r@%h:%p" user@host
+}
+
 function get_env_value {
   echo `sed -n "/$1/p" .env | sed -E "s/$1=//g"`
 }
@@ -28,16 +32,22 @@ reset_bold=`tput rmso`
 
 THE_SITES_PATH=$REMOTE_PATH
 
+if [[ -d ~/.ssh ]]; then
+  if [[ ! -d ~/.ssh/ctl ]]; then
+    mkdir ~/.ssh/ctl
+  fi
+  ssh -p $SSH_PORT -nNf -o ControlMaster=yes -o ControlPath="$HOME/.ssh/ctl/%L-%r@%h:%p" $SSH_USERNAME@$SSH_HOST
+fi
 for d in */ ; do
   if [[ -d $d/$PATH_TO_WORDPRESS ]]; then
     cd "$d"
     set -e
-    trap 'echo "could not copy remote uploads"' INT TERM EXIT
+    trap 'echo "could not copy remote uploads; clean_up"' INT TERM EXIT
     if [[ ! -z $SSH_HOST ]] && [[ ! -z $SSH_USERNAME ]] && [[ ! -z $SSH_PORT ]] && [[ ! -z $REMOTE_PATH ]] && [[ ! -z $PATH_TO_WORDPRESS ]]; then
       PATH_NAME=$(echo $d | sed -E "s/\///g")
       PATH_TO_SITE=$THE_SITES_PATH/$PATH_NAME
       echo "Copying to... ${bold}${red}$PATH_TO_SITE${reset}${reset_bold}"
-      copy-remote-uploads-to-local $SSH_HOST $SSH_USERNAME $SSH_PORT $PATH_TO_SITE $PATH_TO_WORDPRESS
+      copy-remote-uploads-to-local $SSH_HOST $SSH_USERNAME $SSH_PORT $PATH_TO_SITE $PATH_TO_WORDPRESS true
       echo "${bold}${green}Success${reset}${reset_bold}"
     else
       copy-remote-uploads-to-local
@@ -47,5 +57,5 @@ for d in */ ; do
     cd ..
   fi
 done
-
+clean_up
 }
--- imports/copy-remote-uploads-to-local.sh
+++ imports/copy-remote-uploads-to-local.sh
@@ -22,15 +22,27 @@ function copy-remote-uploads-to-local {
   if [[ ! -z $5 ]]; then
     PATH_TO_WORDPRESS=$5
   fi
+  if [[ ! -z $6 ]]; then
+    USE_CONTROLMASTER=$6
+  else
+    USE_CONTROLMASTER=false
+  fi
   rsync_version=`rsync --version | sed -n "/version/p" | sed -E "s/rsync.{1,3}.version //g" | sed -E "s/  protocol version.{1,5}//g"`
   if [[ $rsync_version != '3.1.0' ]]; then
     echo "Warning! You must upgrade rsync. Your rsync version is : $rsync_version"
   fi
   if [ ! -z $PATH_TO_WORDPRESS ] && [ -d $PATH_TO_WORDPRESS ]; then
-    rsync --iconv=UTF-8-MAC,UTF-8 --delete -avz -e "ssh -p $SSH_PORT" --progress $SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_WORDPRESS/wp-content/uploads $PATH_TO_WORDPRESS/wp-content/
-    # scp -rCP $SSH_PORT "$SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_WORDPRESS/wp-content/uploads" $PATH_TO_WORDPRESS/wp-content/
+    if [[ $USE_CONTROLMASTER ]]; then
+      rsync --iconv=UTF-8-MAC,UTF-8 --delete -avz -e "ssh -p $SSH_PORT -o 'ControlPath=$HOME/.ssh/ctl/%L-%r@%h:%p'" --progress $SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_WORDPRESS/wp-content/uploads $PATH_TO_WORDPRESS/wp-content/
+    else
+      rsync --iconv=UTF-8-MAC,UTF-8 --delete -avz -e "ssh -p $SSH_PORT" --progress $SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_WORDPRESS/wp-content/uploads $PATH_TO_WORDPRESS/wp-content/
+    fi
   elif [ ! -z $PATH_TO_DRUPAL ] && [ -d $PATH_TO_DRUPAL ]; then
-    rsync --iconv=UTF-8-MAC,UTF-8 --delete -avz -e "ssh -p $SSH_PORT" --progress $SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_DRUPAL/sites/default/files $PATH_TO_DRUPAL/sites/default/
+    if [[ $USE_CONTROLMASTER ]]; then
+      rsync --iconv=UTF-8-MAC,UTF-8 --delete -avz -e "ssh -p $SSH_PORT -o 'ControlPath=$HOME/.ssh/ctl/%L-%r@%h:%p'" --progress $SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_DRUPAL/sites/default/files $PATH_TO_DRUPAL/sites/default/
+    else
+      rsync --iconv=UTF-8-MAC,UTF-8 --delete -avz -e "ssh -p $SSH_PORT" --progress $SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_DRUPAL/sites/default/files $PATH_TO_DRUPAL/sites/default/
+    fi
     # scp -rCP $SSH_PORT "$SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_DRUPAL/sites/default/files" $PATH_TO_DRUPAL/sites/default/
   else
     echo "Can not find CMS installation. Exiting..."
