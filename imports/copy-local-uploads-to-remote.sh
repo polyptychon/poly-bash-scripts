7d787a4788630e3193782f2b05d7c10436c4a3d0
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-03-02 17:16:55 +0200

    clean up

--- imports/copy-local-uploads-to-remote.sh
+++ imports/copy-local-uploads-to-remote.sh
@@ -54,13 +54,13 @@ function copy-local-uploads-to-remote {
   fi
 
   if [ ! -z $PATH_TO_WORDPRESS ] && [ -d $PATH_TO_WORDPRESS ]; then
-    if [[ $USE_CONTROLMASTER ]]; then
+    if [[ $USE_CONTROLMASTER == true ]]; then
       rsync --iconv=UTF-8-MAC,UTF-8 --delete -avz -e "ssh -p $SSH_PORT -o 'ControlPath=$HOME/.ssh/ctl/%L-%r@%h:%p'" --progress $PATH_TO_WORDPRESS/wp-content/uploads/* $SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_WORDPRESS/wp-content/uploads
     else
       rsync --iconv=UTF-8-MAC,UTF-8 --delete -avz -e "ssh -p $SSH_PORT" --progress $PATH_TO_WORDPRESS/wp-content/uploads/* $SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_WORDPRESS/wp-content/uploads
     fi
   elif [ ! -z $PATH_TO_DRUPAL ] && [ -d $PATH_TO_DRUPAL ]; then
-    if [[ $USE_CONTROLMASTER ]]; then
+    if [[ $USE_CONTROLMASTER == true ]]; then
       rsync --iconv=UTF-8-MAC,UTF-8 --delete -avz -e "ssh -p $SSH_PORT -o 'ControlPath=$HOME/.ssh/ctl/%L-%r@%h:%p'" --progress $PATH_TO_DRUPAL/sites/default/files/* $SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_DRUPAL/sites/default/files
     else
       rsync --iconv=UTF-8-MAC,UTF-8 --delete -avz -e "ssh -p $SSH_PORT" --progress $PATH_TO_DRUPAL/sites/default/files/* $SSH_USERNAME@$SSH_HOST:$REMOTE_PATH/$PATH_TO_DRUPAL/sites/default/files
