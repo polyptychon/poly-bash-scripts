6cb900ee12490dff353167a251e030d82f2e0201
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-03-07 15:11:40 +0200

    clean up

--- imports/check-uploads.sh
+++ imports/check-uploads.sh
@@ -3,7 +3,23 @@
 function get_env_value {
   echo `sed -n "/$1/p" .env | sed -E "s/$1=//g"`
 }
+rawurlencode() {
+  local string="${1}"
+  local strlen=${#string}
+  local encoded=""
+  local pos c o
 
+  for (( pos=0 ; pos<strlen ; pos++ )); do
+     c=${string:$pos:1}
+     case "$c" in
+        [-_.~a-zA-Z0-9] ) o="${c}" ;;
+        * )               printf -v o '%%%02x' "'$c"
+     esac
+     encoded+="${o}"
+  done
+  echo "${encoded}"    # You can either set a return variable (FASTER)
+  REPLY="${encoded}"   #+or echo the result (EASIER)... or both... :p
+}
 function check-uploads() {
 
 if [[ -f .env ]]; then
@@ -36,15 +52,19 @@ fi
 
 trap 'echo -ne "\nExiting..."; cd ..' INT TERM EXIT
 find $PATH_TO_UPLOADS '*.*' | while read FILENAME; do
-  if [[ $FILENAME == $PATH_TO_UPLOADS ]]; then
+  if [[ $FILENAME == $PATH_TO_UPLOADS ]] || [[ -d $FILENAME ]]; then
     continue
   fi
-  if curl --data-urlencode --output /dev/null --silent --head --fail "http://$REMOTE_DOMAIN/$FILENAME"
+  FILE_NAME=`basename "${FILENAME}"`
+  FILE_PATH=`dirname "${FILENAME}"`
+  URL=$FILE_PATH/`rawurlencode "${FILE_NAME}"`
+
+  if curl --output /dev/null --silent --head --fail "http://$REMOTE_DOMAIN/$URL"
   then
     echo -ne "\r                                                                                                                \r"
-    echo -ne "\rFile ${bold}${green} http://$REMOTE_DOMAIN/$FILENAME ${reset}${reset_bold} Exist\r"
+    echo -ne "\rFile ${bold}${green} http://$REMOTE_DOMAIN/$URL ${reset}${reset_bold} Exist\r"
   else
-    echo "File ${bold}${red} http://$REMOTE_DOMAIN/$FILENAME ${reset}${reset_bold} does not Exist"
+    echo "File ${bold}${red} http://$REMOTE_DOMAIN/$URL ${reset}${reset_bold} does not Exist"
   fi
 done
 cd ..
