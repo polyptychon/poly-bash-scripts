a9fb3ac24671214f21c910560e75dbd8610f0c19
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-03-07 14:41:47 +0200

    clean up

--- imports/check-uploads.sh
+++ imports/check-uploads.sh
@@ -24,14 +24,17 @@ reset=`tput sgr0`
 reset_bold=`tput rmso`
 
 if [ ! -z $PATH_TO_WORDPRESS ] && [ -d $PATH_TO_WORDPRESS ]; then
-  PATH_TO_UPLOADS="$PATH_TO_WORDPRESS/wp-content/uploads/"
+  cd $PATH_TO_WORDPRESS
+  PATH_TO_UPLOADS="wp-content/uploads/"
 elif [ ! -z $PATH_TO_DRUPAL ] && [ -d $PATH_TO_DRUPAL ]; then
-  PATH_TO_UPLOADS="$PATH_TO_DRUPAL/sites/default/files/"
+  cd $PATH_TO_DRUPAL
+  PATH_TO_UPLOADS="sites/default/files/"
 else
   echo "Could not find path! Exiting..."
   exit
 fi
 
+trap 'echo -ne "\nExiting..."; cd ..' INT TERM EXIT
 find $PATH_TO_UPLOADS '*.*' | while read FILENAME; do
   if [[ $FILENAME == $PATH_TO_UPLOADS ]]; then
     continue
@@ -39,11 +42,12 @@ find $PATH_TO_UPLOADS '*.*' | while read FILENAME; do
   if curl --output /dev/null --silent --head --fail "http://$REMOTE_DOMAIN/$FILENAME"
   then
     echo -ne "\r                                                                                                                \r"
-    echo -ne "\rFile ${bold}${green}$FILENAME${reset}${reset_bold} Exist\r"
+    echo -ne "\rFile ${bold}${green} http://$REMOTE_DOMAIN/$FILENAME ${reset}${reset_bold} Exist\r"
   else
-    echo "File ${bold}${red}$FILENAME${reset}${reset_bold} does not Exist"
+    echo "File ${bold}${red} http://$REMOTE_DOMAIN/$FILENAME ${reset}${reset_bold} does not Exist"
   fi
 done
+cd ..
 echo -ne '\n'
 
 }
