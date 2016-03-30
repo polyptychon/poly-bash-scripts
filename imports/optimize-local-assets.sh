3a9cd5b488c7b73ad218589a26fba4c6f373eb4e
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-03-30 11:06:52 +0300

    clean up

--- imports/optimize-local-assets.sh
+++ imports/optimize-local-assets.sh
@@ -35,6 +35,9 @@ function optimize-local-assets {
   if [[ -z $PATH_TO_WORDPRESS ]]; then
     PATH_TO_WORDPRESS="wordpress"
   fi
+  if [[ -z $PATH_TO_DRUPAL ]]; then
+    PATH_TO_DRUPAL="drupal_site"
+  fi
 
   if [[ ! -f $POLY_SCRIPTS_FOLDER/ewww/optipng ]] || [[ ! -f $POLY_SCRIPTS_FOLDER/ewww/jpegtran ]]; then
     echo "Could not find optipng or jpegtran! Exiting..."
@@ -59,5 +62,24 @@ function optimize-local-assets {
   #optimize sprite
   if [[ -d ./static/_mockups/sprite ]]; then
     compress-images-in-folder ./static/_mockups/sprite
+  fi
+
+  if [[ -d ./$PATH_TO_DRUPAL/sites/all/themes ]]; then
+    for img in `find ./$PATH_TO_DRUPAL/sites/all/themes/ -type f -name "*.png" -or -name "*.jpg"`; do
+      echo "crushing $img ..."
+      set +e
+      if [ ${img: -4} == ".png" ]; then
+        $POLY_SCRIPTS_FOLDER/ewww/optipng -o2 -strip all "$img"
+      elif [ ${img: -4} == ".jpg" ]; then
+        $POLY_SCRIPTS_FOLDER/ewww/jpegtran -copy none -optimize -perfect "$img" > temp.jpg
+        if [ $? = 0 ] && [ -f temp.jpg ]; then
+          mv -f temp.jpg "$img"
+        fi
+      fi
+      if [ -f temp.jpg ]; then
+        rm temp.jpg
+      fi
+      set -e
+    done
   fi
 }
