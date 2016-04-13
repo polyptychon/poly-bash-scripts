b47a82e3d8516d65c41c6fa669a3e8bf49a3d433
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-04-13 14:24:45 +0300

    symlink drupal sites

--- imports/all-symlink-uploads.sh
+++ imports/all-symlink-uploads.sh
@@ -20,12 +20,19 @@ if [[ -z $PATH_TO_WORDPRESS ]]; then
   PATH_TO_WORDPRESS=wordpress
 fi
 
+if [[ -z $PATH_TO_DRUPAL ]]; then
+  PATH_TO_DRUPAL="drupal_site"
+fi
+
 if [[ -z $PATH_TO_UPLOADS ]]; then
   PATH_TO_UPLOADS="$PATH_TO_WORDPRESS/wp-content/uploads"
 fi
 
 if [[ -z $PATH_TO_WORDPRESS_SITES ]]; then
   PATH_TO_WORDPRESS_SITES="/Volumes/POLYPTYCHON/Work/wordpress-sites"
+fi
+if [[ -z $PATH_TO_DRUPAL_SITES ]]; then
+  PATH_TO_DRUPAL_SITES="/Volumes/POLYPTYCHON/Work/drupal-sites"
 fi
 
 bold=`tput bold`
@@ -41,20 +48,33 @@ if [[ $answer =~ ^[Nn]$ ]]; then
   exit
 fi
 
-echo -n " # Path to projects ($PATH_TO_WORDPRESS_SITES): "
+echo -n " # Path to wordpress projects ($PATH_TO_WORDPRESS_SITES): "
 read PATH_TO_WORDPRESS_SITES_TEMP
 if [ ! -z ${PATH_TO_WORDPRESS_SITES_TEMP} ]; then
   PATH_TO_WORDPRESS_SITES=$PATH_TO_WORDPRESS_SITES_TEMP
 fi
 
+echo -n " # Path to drupal projects ($PATH_TO_DRUPAL_SITES): "
+read PATH_TO_DRUPAL_SITES_TEMP
+if [ ! -z ${PATH_TO_DRUPAL_SITES_TEMP} ]; then
+  PATH_TO_DRUPAL_SITES=$PATH_TO_DRUPAL_SITES_TEMP
+fi
+
 for d in */ ; do
   PATH_NAME=$(echo $d | sed -E "s/\///g")
-  if [[ ! -d $d/$PATH_TO_WORDPRESS ]]; then
-    echo "$d is not a wordpress site"
+
+  if [ ! -z $PATH_TO_WORDPRESS ] && [ -d $d/$PATH_TO_WORDPRESS ]; then
+    PATH_TO_UPLOADS="$PATH_TO_WORDPRESS/wp-content/uploads"
+    PATH_TO_SITES=$PATH_TO_WORDPRESS_SITES
+  elif [ ! -z $PATH_TO_DRUPAL ] && [ -d $d/$PATH_TO_DRUPAL ]; then
+    PATH_TO_UPLOADS="$PATH_TO_DRUPAL/sites/default/files"
+    PATH_TO_SITES=$PATH_TO_DRUPAL_SITES
+  else
+    echo "$d is not a wordpress or drupal site"
     continue
   fi
-  if [[ ! -d $PATH_TO_WORDPRESS_SITES/$PATH_NAME/$PATH_TO_UPLOADS ]]; then
-    echo "$PATH_TO_WORDPRESS_SITES/$PATH_NAME/$PATH_TO_UPLOADS is not a valid path"
+  if [[ ! -d $PATH_TO_SITES/$PATH_NAME/$PATH_TO_UPLOADS ]]; then
+    echo "$PATH_TO_SITES/$PATH_NAME/$PATH_TO_UPLOADS is not a valid path"
     continue
   fi
   if [[ -d $d/$PATH_TO_UPLOADS ]]; then
@@ -63,7 +83,7 @@ for d in */ ; do
   if [[ -f $d/$PATH_TO_UPLOADS ]]; then
     rm $d/$PATH_TO_UPLOADS
   fi
-  ln -sf $PATH_TO_WORDPRESS_SITES/$PATH_NAME/$PATH_TO_UPLOADS $PATH_NAME/$PATH_TO_UPLOADS
+  ln -sf $PATH_TO_SITES/$PATH_NAME/$PATH_TO_UPLOADS $PATH_NAME/$PATH_TO_UPLOADS
   echo "${bold}${green}Success${reset}${reset_bold} $PATH_NAME"
 done
 
