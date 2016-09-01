2f1f9fbb7c6f1c9e31f5287da1e68196cbc264a4
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-09-01 10:43:04 +0300

    clean up

--- imports/backup-remote-sites-light.sh
+++ imports/backup-remote-sites-light.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 function backup-remote-sites-light {
+  if [ ! -z $2 ] && [ -d "$1 $2" ]; then
+    echo $1 $2
+    cd "$1 $2"
+  elif [ ! -z $1 ] && [ -d $1 ]; then
+    echo $1
+    cd $1
+  fi
+
   now="$(date +'%d/%m/%Y')"
   set +e
   all-dump-remote-db "n"
