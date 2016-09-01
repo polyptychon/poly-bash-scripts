6b8b00da0a80e2f0ce29c597c0d42af0efd85b8a
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-09-01 10:37:50 +0300

    clean up

--- imports/backup-remote-sites-light.sh
+++ imports/backup-remote-sites-light.sh
@@ -3,7 +3,7 @@
 function backup-remote-sites-light {
   now="$(date +'%d/%m/%Y')"
   set +e
-  all-dump-remote-db
+  all-dump-remote-db "n"
   all-copy-remote-uploads-to-local "n"
   git add --all
   git commit -m "backup at $now"
