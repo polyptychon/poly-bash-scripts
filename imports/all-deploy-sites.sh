3eea79cfcd4bbdd0f1f5ec6dfa76447c4a81ae37
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2017-01-17 11:29:49 +0200

    update deploy all sites script

--- imports/all-deploy-sites.sh
+++ imports/all-deploy-sites.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 function get_env_value {
-  echo `sed -n "/$1/p" .env | sed -E "s/$1=//g"`
-}
-function clean_up() {
-  if [[ -d .git ]]; then
-    set +e
-    git stash pop --quiet
-    cd ..
-    set -e
+  if [[ -z $2 ]]; then
+    ENV=.env
+  else
+    ENV=$2
   fi
+  echo `sed -n "/$1/p" $ENV | sed -E "s/$1=//g"`
 }
 
-function all-deploy-sites() {
+function all-deploy-sites {
 
 if [[ -f .env ]]; then
   source .env
@@ -24,8 +21,27 @@ else
   exit
 fi
 
+if [[ ! -z $1 ]]; then
+  ASK_FOR_CONFIRMATION=$1
+else
+  ASK_FOR_CONFIRMATION="y"
+fi
+
+LOCAL_PATHS=()
+
+if [[ -z $REMOTE_PATH ]] && [[ ! -z REMOTE_SSH_ROOT_PATH ]]; then
+  REMOTE_PATH=$REMOTE_SSH_ROOT_PATH
+else
+  echo "REMOTE_PATH variable is not set!"
+  exit
+fi
+
 if [[ -z $PATH_TO_WORDPRESS ]]; then
   PATH_TO_WORDPRESS="wordpress"
+fi
+
+if [[ -z $PATH_TO_DRUPAL ]]; then
+  PATH_TO_DRUPAL="drupal_site"
 fi
 
 bold=`tput bold`
@@ -34,70 +50,55 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 reset_bold=`tput rmso`
 
-echo -n "You are about to ${bold}${red}deploy all sites${reset}${reset_bold} to host ${bold}${red}$SSH_HOST${reset}${reset_bold}. Are you sure? Y/N "
-read answer
-if [[ $answer =~ ^[Nn]$ ]]; then
-  exit
-fi
-
-if [[ -d ~/.ssh ]]; then
-  if [[ ! -d ~/.ssh/ctl ]]; then
-    mkdir ~/.ssh/ctl
+if [[ $ASK_FOR_CONFIRMATION =~ ^[Yy]$  ]]; then
+  echo -n "You are about to ${bold}${red}deploy all sites${reset}${reset_bold} to host ${bold}${red}$SSH_HOST${reset}${reset_bold}. Are you sure? Y/N "
+  read REPLY
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo "Exiting..."
+    exit
   fi
-  ssh -p $SSH_PORT -nNf -o ControlMaster=yes -o ControlPath="$HOME/.ssh/ctl/%L-%r@%h:%p" $SSH_USERNAME@$SSH_HOST
 fi
+
 for d in */ ; do
-  if [[ -d $d/$PATH_TO_WORDPRESS ]]; then
-    cd "$d"
-    set -e
-    trap 'echo "could not read .env"; clean_up' INT TERM EXIT
-
-    REMOTE_PATH=`get_env_value "REMOTE_PATH"`
-    REMOTE_PATH_LOWER=$(echo $REMOTE_PATH | tr '[:upper:]' '[:lower:]')
-    echo ${bold}${green}$REMOTE_PATH${reset}${reset_bold}
-
+  if [[ -d $d/$PATH_TO_WORDPRESS ]] || [[ -d $d/$PATH_TO_DRUPAL ]]; then
+    PATH_NAME=$(echo $d | sed -E "s/\///g")
+    LOCAL_PATHS+=($PATH_NAME)
     set +e
     git stash --quiet
     git pull --quiet
     set -e
 
     set -e
-    trap 'echo "could not push"; clean_up' INT TERM EXIT
+    trap 'echo "could not push"; git stash pop --quiet' INT TERM EXIT
     git push
-
-    set +e
-    LOCAL_COMMIT_HASH=$(git rev-parse HEAD)
-    ssh -t -p $SSH_PORT -o 'ControlPath=$HOME/.ssh/ctl/%L-%r@%h:%p' $SSH_USERNAME@$SSH_HOST bash -c "'
-      if [[ -d $REMOTE_PATH ]] || [[ -d $REMOTE_PATH_LOWER ]]; then
-        if [[ -d $REMOTE_PATH ]]; then
-          cd $REMOTE_PATH
-        elif [[ -d $REMOTE_PATH_LOWER ]]; then
-          cd $REMOTE_PATH_LOWER
-        fi
-
-        REMOTE_COMMIT_HASH=\$(git rev-parse HEAD)
-        if [ $LOCAL_COMMIT_HASH == \$REMOTE_COMMIT_HASH ]; then
-          echo \"Everything is up to date. No action is required\"
-          exit
-        else
-          git stash clear
-          git stash --quiet
-          git status
-          git pull
-          git stash pop --quiet
-          exit
-        fi
-      else
-        echo \"Could not find path $REMOTE_PATH in remote server\"
-      fi
-    '"
-    set +e
-    git stash pop --quiet
-    set -e
-    echo
-    trap 'echo "OK"; clean_up' INT TERM EXIT
-    cd ..
   fi
 done
+
+ssh -T -p $SSH_PORT $SSH_USERNAME@$SSH_HOST <<EOF
+  if [[ ! -d $PATH_TO_TEMP_EXPORTS ]]; then
+    mkdir $PATH_TO_TEMP_EXPORTS
+  fi
+  cd $REMOTE_PATH
+  for d in ${LOCAL_PATHS[@]}; do
+    dl=\$(echo \$d | tr '[:upper:]' '[:lower:]')
+    if [[ -d \$d ]] || [[ -d \$dl ]]; then
+      if [[ -d \$d ]]; then
+        cd \$d
+      elif [[ -d \$dl ]]; then
+        cd \$dl
+      fi
+
+      echo \$d
+      git stash clear
+      git stash --quiet
+      git status
+      git pull
+      git stash pop --quiet
+
+      cd ..
+    fi
+  done
+  exit
+EOF
 
 }
